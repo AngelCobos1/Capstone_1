57da421e5b61ce021b1f5cbf0311bbb46cd4ee76
Angel <70175639+DeLosLobos@users.noreply.github.com>
2021-03-14 22:36:28 -0500

    update

---
+++ RevisedProject_2021/drafts_script.py
@@ -0,0 +1,75 @@
+import time
+
+import pandas as pd
+import requests
+from sqlalchemy import create_engine
+
+# Create function to pull every season
+
+
+def sourcing_NBA(years):
+    time.sleep(1)  # one second delay on each pull request
+    source = []  # create empty list
+    for i in years:  # iterate through all years in input
+        headers = {
+            "Connection": "keep-alive",
+            "Accept": "application/json, text/plain, */*",
+            "x-nba-stats-token": "true",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36",
+            "x-nba-stats-origin": "stats",
+            "Origin": "https://www.nba.com",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            "Referer": "https://www.nba.com/",
+            "Accept-Language": "en-US,en;q=0.9",
+        }
+
+        params = (
+            ("College", ""),
+            ("LeagueID", "00"),
+            ("OverallPick", ""),
+            ("RoundNum", ""),
+            ("RoundPick", ""),
+            ("Season", i),  # this is where the magic happens
+            ("TeamID", "0"),
+            ("TopX", ""),
+        )
+
+        source.append(
+            requests.get(
+                "https://stats.nba.com/stats/drafthistory",
+                headers=headers,
+                params=params,
+            ).json()
+        )  # append every request to the empty source list
+    return source
+
+
+# NB. Original query string below. It seems impossible to parse and
+# reproduce query strings 100% accurately so the one below is given
+# in case the reproduced version is not "correct".
+# response = requests.get('https://stats.nba.com/stats/drafthistory?College=&LeagueID=00&OverallPick=&RoundNum=&RoundPick=&Season=2020&TeamID=0&TopX=', headers=headers)
+
+
+def make_df(years):
+    # previous function to extract every season of drafts
+    data = sourcing_NBA(years)
+    filtered_list = [data[i]["resultSets"][0]["rowSet"] for i in range(0, 11)]
+    df = pd.concat([pd.DataFrame(filtered_list[i]) for i in range(0, 11)], axis=0)
+
+    column_names = data[0]["resultSets"][0]["headers"]
+    df.columns = column_names
+    return df
+
+
+years = [str(i) for i in range(2010, 2021)]
+
+NBA_Drafts = make_df(years)
+
+NBA_Drafts.to_csv(r"C:\Users\ASUS\Thinkful_Projects\Capstone_1\NBA_Drafts.csv")
+
+NBA_Drafts.columns = NBA_Drafts.columns.str.lower()
+engine = create_engine("postgresql://postgres:mybigdata@localhost:5433/nba")
+NBA_Drafts.to_sql("drafts", con=engine, if_exists="append", index=False)
+engine.dispose()
---
+++ RevisedProject_2021/stats_script.py
@@ -0,0 +1,131 @@
+import time
+
+import pandas as pd
+import requests
+from sqlalchemy import create_engine
+
+
+def request_seasons(seasons):
+    time.sleep(1)  # one second delay on each pull request
+    source = []  # create empty list
+    for i in seasons:  # iterate through all years in input
+        headers = {
+            "Connection": "keep-alive",
+            "Accept": "application/json, text/plain, */*",
+            "x-nba-stats-token": "true",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+            "x-nba-stats-origin": "stats",
+            "Origin": "https://www.nba.com",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            "Referer": "https://www.nba.com/",
+            "Accept-Language": "en-US,en;q=0.9",
+            "If-Modified-Since": "Sun, 14 Feb 2021 03:05:20 GMT",
+        }
+
+        params = (
+            ("College", ""),
+            ("Conference", ""),
+            ("Country", ""),
+            ("DateFrom", ""),
+            ("DateTo", ""),
+            ("Division", ""),
+            ("DraftPick", ""),
+            ("DraftYear", ""),
+            ("GameScope", ""),
+            ("GameSegment", ""),
+            ("Height", ""),
+            ("LastNGames", "0"),
+            ("LeagueID", "00"),
+            ("Location", ""),
+            ("MeasureType", "Base"),
+            ("Month", "0"),
+            ("OpponentTeamID", "0"),
+            ("Outcome", ""),
+            ("PORound", "0"),
+            ("PaceAdjust", "N"),
+            ("PerMode", "PerGame"),
+            ("Period", "0"),
+            ("PlayerExperience", ""),
+            ("PlayerPosition", ""),
+            ("PlusMinus", "N"),
+            ("Rank", "N"),
+            ("Season", i),
+            ("SeasonSegment", ""),
+            ("SeasonType", "Regular Season"),
+            ("ShotClockRange", ""),
+            ("StarterBench", ""),
+            ("TeamID", "0"),
+            ("TwoWay", "0"),
+            ("VsConference", ""),
+            ("VsDivision", ""),
+            ("Weight", ""),
+        )
+
+        source.append(
+            requests.get(
+                "https://stats.nba.com/stats/leaguedashplayerstats",
+                headers=headers,
+                params=params,
+            ).json()
+        )
+    return source
+
+
+# NB. Original query string below. It seems impossible to parse and
+# reproduce query strings 100% accurately so the one below is given
+# in case the reproduced version is not "correct".
+# response = requests.get('https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2020-21&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=', headers=headers)
+
+
+seasons = [
+    "2010-11",
+    "2011-12",
+    "2012-13",
+    "2013-14",
+    "2014-15",
+    "2015-16",
+    "2016-17",
+    "2017-18",
+    "2018-19",
+    "2019-20",
+    "2020-21",
+]
+
+# def request_seasons(years):
+# previous function to extract every season of drafts
+data = request_seasons(seasons)
+
+# Create function to append NBA Season to first player row within stats list, as the last element in list
+
+
+def insert_seasons(nested_list):
+    return [
+        data[i]["resultSets"][0]["rowSet"][0].append((data[i]["parameters"]["Season"]))
+        for i in range(0, 11)
+    ]
+
+
+insert_seasons(data)
+
+
+def make_df(data):
+    filtered_list = [data[i]["resultSets"][0]["rowSet"] for i in range(0, 11)]
+    df = pd.concat([pd.DataFrame(filtered_list[i]) for i in range(0, 11)], axis=0)
+    # Rename columns
+    column_names = data[0]["resultSets"][0]["headers"] + ["SEASON"]
+    df.columns = column_names
+    # Forward-fill the null values in the season column
+    df.loc[:, ["SEASON"]] = df.loc[:, ["SEASON"]].ffill()
+    return df
+
+
+NBA_Stats = make_df(data)
+
+NBA_Stats.to_csv(r"C:\Users\ASUS\Thinkful_Projects\Capstone_1\NBA_Drafts.csv")
+
+NBA_Stats.columns = NBA_Stats.columns.str.lower()
+engine = create_engine("postgresql://postgres:mybigdata@localhost:5433/nba")
+NBA_Stats.to_sql("stats", con=engine, if_exists="append", index=False)
+engine.dispose()
